e9907f8811282ae85ce3472f47f2b431bdc56f48
Abhishek Dwivedi <Abhidwivedi2206@gmail.com>
2025-08-01 23:41:24 +0530

    2D Array using literals

---
+++ 2DusingLiterals.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class UsingLiterals{
+    static void printArray(int arr[][]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+     int[][] arr={
+        {1,2,3},
+        {4,5,6},
+     };
+        printArray(arr);
+    }
+
+}
